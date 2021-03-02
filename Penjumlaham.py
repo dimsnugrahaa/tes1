504a66b6372e5c068c7d19b4558444c795df45f8
Dimas Nugraha <56537138+dimsnugrahaa@users.noreply.github.com>
2021-03-02 11:17:01 +0700

    Update and rename README.md to Penjumlaham.py

---
+++ Penjumlaham.py
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x= 100;
+	int y= 2;
+
+	cout<<" Penjumlahn x dan y "<<endl;
+	cout<<"x = "<<x<<endl;
+	cout<<"y = "<<y<<endl;
+	cout"\n";
+
+	cout<<"Hasil Penjumlahan = "<<x+y<<;
+}
