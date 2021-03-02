13b01f0caeffbcb20870b84d176e571cf6e2e139
Dimas Nugraha <56537138+dimsnugrahaa@users.noreply.github.com>
2021-03-02 11:26:50 +0700

    Update Penjumlaham.py

--- Penjumlaham.py
+++ Penjumlaham.py
@@ -3,13 +3,14 @@ using namespace std;
 
 int main()
 {
-	int x= 100;
-	int y= 2;
-
-	cout<<" Penjumlahn x dan y "<<endl;
-	cout<<"x = "<<x<<endl;
-	cout<<"y = "<<y<<endl;
-	cout"\n";
-
-	cout<<"Hasil Penjumlahan = "<<x+y<<;
+	  int x= 100;
+	  int y=2;
+	
+	  cout<<" Penjumlahan x dan y "<<endl;
+	  cout<<"x ="<<x<<endl;
+	  cout<<"y ="<<y<<endl;
+	  cout"\";
+	
+	  cout<<"Hasil Penjumlahan =
+"<<x+y<<;
 }
